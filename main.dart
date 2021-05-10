b8c89789b5460dfd2ec249b3080795486500b0cf
Muhammad_Sameer <muhammadsameer.css@gmail.com>
2021-05-10 12:41:15 -0700

    Assignment 2  main.dart file

---
+++ main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Sameer());
+}
+
+class Sameer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: (Scaffold(
+        body: Text("Assalam o Alikum Sir,\nI am Sameer"),
+      )),
+    );
+  }
+}
